3f0294c9487ea7c4be24117ce04cfc5cefb2612c
Brady Peterson <bpeterson1663@gmail.com>
2022-01-23 14:26:43 -0600

    feat(wines): call getAllWines on correct page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Container, Typography } from '@mui/material'
-import { getWines } from '../api'
-const Home = () => {
-  const getAllWines = async () => {
-    const response = await getWines()
-    console.log('response: ', response)
-  }
-  useEffect(() => {
-    getAllWines()
-  }, [])
-  return (
-    <Container component="main">
-      <Typography component="header">Home</Typography>
-    </Container>
-  )
-}
+
+const Home = () => (
+  <Container component="main">
+    <Typography component="header">Home</Typography>
+  </Container>
+)
 
 export default Home
--- src/pages/Wines.tsx
+++ src/pages/Wines.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { Container, Typography } from '@mui/material'
 
-const Wines = () => (
-  <Container component="main">
-    <Typography component="header">Wines</Typography>
-  </Container>
-)
+import React, { useEffect } from 'react'
+import { Container, Typography } from '@mui/material'
+import { getWines } from '../api'
+
+const Wines = () => {
+  const getAllWines = async () => {
+    const response = await getWines()
+    console.log('response: ', response)
+  }
+  useEffect(() => {
+    getAllWines()
+  }, [])
+  return (
+    <Container component="main">
+      <Typography component="header">Wines</Typography>
+    </Container>
+  )
+}
 
 export default Wines
