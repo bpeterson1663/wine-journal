fd146e20ebcc4b9d85848f04af9d3b266cebfad3
Brady Peterson <bpeterson1663@gmail.com>
2023-08-05 22:49:05 -0500

    feature(eslint): configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,44 +1,47 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "paths": ["src"]
+      }
     },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  },
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'standard-with-typescript',
+    'plugin:react/jsx-runtime',
+    "react-app"
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
     },
-    "plugins": [
-        "react"
-    ],
-    "parserOptions": {
-        "project": ["./tsconfig.json"]
-    },
-    "rules": {
-        'strict': 0,
-        'indent': [2, 2, { 'SwitchCase': 1 }],
-        'quotes': [2, 'single'],
-        'linebreak-style': [2, 'unix'],
-        'semi': [2, 'never'],
-        'space-before-blocks': 2,
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'parserOptions': {
+    'project': ['./tsconfig.json'],
+  },
+  'rules': {
+    'strict': 0,
+    'quotes': [2, 'single'],
+    'linebreak-style': [2, 'unix'],
+    'semi': [2, 'never'],
+    'space-before-blocks': 2,
     'space-infix-ops': 2,
-    'space-before-function-paren': [2, { 'anonymous': 'always', 'named': 'never' }],
     'array-bracket-spacing': [2, 'never'],
     'object-curly-spacing': [2, 'always'],
     'space-in-parens': [2, 'never'],
@@ -46,7 +49,6 @@ module.exports = {
     'brace-style': [2, '1tbs', { 'allowSingleLine': true }],
     'comma-spacing': [2, { 'before': false, 'after': true }],
     'comma-style': [2, 'last'],
-    'comma-dangle': [2, 'always-multiline'],
     'prefer-arrow-callback': [2, { 'allowNamedFunctions': true }],
     'prefer-const': 1,
     'react/no-multi-comp': [2, { 'ignoreStateless': true }],
@@ -55,7 +57,6 @@ module.exports = {
     'react/prop-types': 2,
     'no-extra-boolean-cast': 0,
     'no-console': ['error', { 'allow': ['warn', 'error'] }],
-    'no-relative-import-paths/no-relative-import-paths': ['error', { 'allowSameFolder': true, 'rootDir': 'app' }],
     'react/jsx-curly-spacing': [2, { 'when': 'always', 'children': true }],
     'react/jsx-curly-brace-presence': [2, { 'props': 'never', 'children': 'never' }],
     'react/no-unused-prop-types': 2,
@@ -68,15 +69,15 @@ module.exports = {
       }],
     },
     ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    // 'max-len': ['warn', { 'code': 120 }],
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'import/first': 'error',
-    'import/no-duplicates': 'error',
-    'import/newline-after-import': 'error',
-    'no-redeclare': 'off',
-    '@typescript-eslint/no-redeclare': 'error',
-    }
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/strict-boolean-expressions": ["error", {
+      'allowString': true, 
+      'allowNumber': true,
+      'allowNullableBoolean': true,
+      'allowNullableString': true,
+      'allowNullableNumber': true,
+    }],
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+  },
 }
