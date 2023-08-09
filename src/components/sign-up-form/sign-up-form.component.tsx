cc0a478cdb6568cfad3a5793eb9419b041887882
Brady Peterson <bpeterson1663@gmail.com>
2023-08-08 21:48:24 -0500

    feature(auth): style sign up form

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -1,15 +1,22 @@
 import { Button, Container, TextField } from '@mui/material'
+import { styled } from '@mui/material/styles'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
-import { signUp } from '../../features/auth/authSlice'
-import { useAppDispatch } from '../../features/hooks'
-import { SignUpT } from '../../types'
+import { signUp } from 'features/auth/authSlice'
+import { useAppDispatch } from 'features/hooks'
+import { SignUpT } from 'types'
+
 interface SignUpFormT extends SignUpT {
   confirmPassword: string
 }
 
+const StyledTextField = styled(TextField)({
+  margin: '8px 0'
+})
+
 const SignUpForm = () => {
-  const { handleSubmit, control } = useForm<SignUpFormT>()
+  const { handleSubmit, control, formState } = useForm<SignUpFormT>()
   const dispatch = useAppDispatch()
+  const { errors } = formState
 
   const onSubmitHandler: SubmitHandler<SignUpFormT> = async data => {
     const { password, confirmPassword, email, firstName, lastName } = data
@@ -33,39 +40,94 @@ const SignUpForm = () => {
       <Controller
         name="firstName"
         control={ control }
+        rules={ {
+          required: '*Required'
+        } }
         defaultValue=""
-        render={ ({ field }) => <TextField id="firstName" label="First Name" { ...field } /> }
+        render={ ({ field }) => (
+          <StyledTextField
+          { ...field }
+            id="firstName"
+            label="First Name"
+            error={ !!errors.firstName }
+            helperText={ errors.firstName ? errors.firstName?.message : '' }
+          />
+        ) }
       />
       <Controller
         name="lastName"
         control={ control }
+        rules={ {
+          required: '*Required'
+        } }
         defaultValue=""
-        render={ ({ field }) => <TextField id="lastName" label="Last Name" { ...field } /> }
+        render={ ({ field }) => (
+          <StyledTextField
+            { ...field }
+            id="lastName"
+            label="Last Name"
+            error={ !!errors.lastName }
+            helperText={ errors.lastName ? errors.lastName?.message : '' }
+          />
+        ) }
       />
       <Controller
         name="email"
         control={ control }
+        rules={ {
+          required: '*Required'
+        } }
         defaultValue=""
-        render={ ({ field }) => <TextField id="email" type="email" label="Email" { ...field } /> }
+        render={ ({ field }) => (
+          <StyledTextField
+            { ...field }
+            id="email"
+            type="email"
+            label="Email"
+            autoComplete="on"
+            error={ !!errors.email }
+            helperText={ errors.email ? errors.email?.message : '' }
+          />
+        ) }
       />
       <Controller
         name="password"
         control={ control }
+        rules={ {
+          required: '*Required'
+        } }
         defaultValue=""
         render={ ({ field }) => (
-          <TextField autoComplete="on" id="password" type="password" label="Password" { ...field } />
+          <StyledTextField
+            { ...field }
+            id="password"
+            type="password"
+            label="Password"
+            error={ !!errors.password }
+            helperText={ errors.password ? errors.password?.message : '' }
+          />
         ) }
       />
       <Controller
         name="confirmPassword"
         control={ control }
+        rules={ {
+          required: '*Required'
+        } }
         defaultValue=""
         render={ ({ field }) => (
-          <TextField autoComplete="on" id="confirmPassword" type="password" label="Confirm Password" { ...field } />
+          <StyledTextField
+            { ...field }
+            id="confirmPassword"
+            type="password"
+            label="Confirm Password"
+            error={ !!errors.confirmPassword }
+            helperText={ errors.confirmPassword ? errors.confirmPassword?.message : '' }
+          />
         ) }
       />
       <Button type="submit" variant="contained" sx={ { mt: 1, mr: 1 } }>
-        Submit
+        Sign Up
       </Button>
     </Container>
   )
